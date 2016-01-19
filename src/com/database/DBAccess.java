46e4d0b4216841dac06d52f277d3dd5fcc1e4596
Ibrahiiim <ibrahimabuaquel@gmail.com>
2016-01-19 19:08:21 +0300

    Database Connection ==> start up getting data from table

---
+++ src/com/database/DBAccess.java
@@ -0,0 +1,27 @@
+package com.database;
+
+import java.sql.*;
+
+/**
+ * Created by Ibrahim Abuaqel on 1/19/2016.
+ */
+public class DBAccess {
+
+    private Connection conn;
+    private Statement stmt;
+    private ResultSet result;
+
+    public void connect() throws ClassNotFoundException, SQLException {
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        conn=DriverManager.getConnection("jdbc:mysql://localhost/abetasdb?user=root&password=abetas");
+        stmt=conn.createStatement();
+        result=stmt.executeQuery("select * from university");
+
+        while (result.next()){
+            System.out.println(result.getString(1)+" "+result.getString(2));
+        }
+    }
+
+}
