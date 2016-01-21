9eec36303fdeb9eb40e41a529018194422ed7054
Ibrahiiim <ibrahimabuaquel@gmail.com>
2016-01-21 17:59:43 +0300

    DB Forms

--- src/com/database/Delete.java
+++ src/com/database/Delete.java
@@ -12,13 +12,8 @@ import java.io.PrintWriter;
  * Created by Ibrahim Abuaqel on 1/19/2016.
  */
 @WebServlet(name = "Delete",
-<<<<<<< HEAD:src/com/database/Delete.java
         urlPatterns = {"/Delete"})
 public class Delete extends HttpServlet {
-=======
-        urlPatterns = {"Delete1"})
- public class Delete extends HttpServlet {
->>>>>>> fafb41f6f6453ffb2c87d123e10635d2b3f6a1ae:src/com/database/Delete.java
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("##########################################################");
         DBAccess dba=new DBAccess();
