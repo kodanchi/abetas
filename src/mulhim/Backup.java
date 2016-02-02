1f8b88bb1cc901f550bd96624a91fac5272b1ceb
unknown <Mohammed@surface.mulhim2.com>
2016-02-02 22:30:00 +0300

    Done with backup but still didn't do the recovery

--- src/mulhim/Backup.java
+++ src/mulhim/Backup.java
@@ -1,6 +1,5 @@
 package mulhim;
 
-import javax.jws.WebService;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,13 +9,70 @@ import java.io.IOException;
 
 /**
  * Created by Mohammed on 2/1/2016.
- * https://malalanayake.wordpress.com/2012/09/28/take-the-mysql-db-backup-in-java/
+ * http://www.coderanch.com/t/480353/JDBC/databases/MySql-DB-backup-java
  *
  */
 @WebServlet(name = "backup",
         urlPatterns = {"/Backup"})
 public class Backup extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        //backupDB();
+        restoreDB();
     }
+
+    String dbName = "abetasdb";
+    String dbUser = "root";
+    String dbPass = "abetas";
+
+
+    public void backupDB() {
+        String executeCmd = "";
+
+        executeCmd = "C:\\Program Files\\MySQL\\MySQL Server 5.7\\bin\\mysqldump.exe -u " + dbUser + " -p" + dbPass + " " + dbName + " -r backup_test2.sql";
+
+        Process runtimeProcess = null;
+        try {
+            runtimeProcess = Runtime.getRuntime().exec(executeCmd);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int processComplete = 0;
+        try {
+            processComplete = runtimeProcess.waitFor();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (processComplete == 0) {
+            System.out.println("Backup taken successfully");
+        } else {
+            System.out.println("Could not take mysql backup");
+        }
+    }
+
+    public void restoreDB() {
+
+String path="E:\\L 8\\Project Implementation\\apache-tomee-1.7.3-plus\\apache-tomee-plus-1.7.3\\bin\\backup.sql";
+            String executeCmd = "C:\\Program Files\\MySQL\\MySQL Server 5.7\\bin\\mysql.exe -u + dbUser + \" -p\" + dbPass + \" \" + dbName + <" + path;
+
+            System.out.println(executeCmd);
+
+            Process runtimeProcess= null;
+
+            try {
+                runtimeProcess = Runtime.getRuntime().exec(executeCmd);
+
+                int processComplete = runtimeProcess.waitFor();
+
+                System.out.println(processComplete);
+
+                if (processComplete == 0) {
+                    System.out.println("Restored the Backup !");
+                } else {
+                    System.out.println("Couldn't Restore the backup !");
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+        }
     }
