3e87c826e1c92ee6762a1795d754cb7345d068b7
unknown <Mohammed@surface.mulhim2.com>
2016-02-03 21:13:59 +0300

    done with restore

--- src/mulhim/Backup.java
+++ src/mulhim/Backup.java
@@ -10,13 +10,14 @@ import java.io.IOException;
 /**
  * Created by Mohammed on 2/1/2016.
  * http://www.coderanch.com/t/480353/JDBC/databases/MySql-DB-backup-java
+ * http://ubuntuforums.org/showthread.php?t=1184395
  *
  */
 @WebServlet(name = "backup",
         urlPatterns = {"/Backup"})
 public class Backup extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //backupDB();
+       // backupDB();
         restoreDB();
     }
 
@@ -28,7 +29,7 @@ public class Backup extends HttpServlet {
     public void backupDB() {
         String executeCmd = "";
 
-        executeCmd = "C:\\Program Files\\MySQL\\MySQL Server 5.7\\bin\\mysqldump.exe -u " + dbUser + " -p" + dbPass + " " + dbName + " -r backup_test2.sql";
+        executeCmd = "C:\\Program Files\\MySQL\\MySQL Server 5.7\\bin\\mysqldump.exe -u " + dbUser + " -p" + dbPass + " " + dbName + " -r test_backup.sql";
 
         Process runtimeProcess = null;
         try {
@@ -50,29 +51,26 @@ public class Backup extends HttpServlet {
     }
 
     public void restoreDB() {
-
-String path="E:\\L 8\\Project Implementation\\apache-tomee-1.7.3-plus\\apache-tomee-plus-1.7.3\\bin\\backup.sql";
-            String executeCmd = "C:\\Program Files\\MySQL\\MySQL Server 5.7\\bin\\mysql.exe -u + dbUser + \" -p\" + dbPass + \" \" + dbName + <" + path;
-
-            System.out.println(executeCmd);
+        String[] executeCmd = new String[]{"C:\\Program Files\\MySQL\\MySQL Server 5.7\\bin\\mysql.exe", dbName, "--user=" + dbUser, "--password=" + dbPass, "-e", " source C:\\Program Files\\MySQL\\MySQL Server 5.7\\bin\\test_backup.sql" };
+            //System.out.println(executeCmd);
 
             Process runtimeProcess= null;
 
-            try {
-                runtimeProcess = Runtime.getRuntime().exec(executeCmd);
-
-                int processComplete = runtimeProcess.waitFor();
-
-                System.out.println(processComplete);
-
-                if (processComplete == 0) {
-                    System.out.println("Restored the Backup !");
-                } else {
-                    System.out.println("Couldn't Restore the backup !");
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-
+        try {
+            runtimeProcess = Runtime.getRuntime().exec(executeCmd);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int processComplete = 0;
+        try {
+            processComplete = runtimeProcess.waitFor();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (processComplete == 0) {
+            System.out.println("Restore taken successfully");
+        } else {
+            System.out.println("restore failure");
+        }
         }
     }
