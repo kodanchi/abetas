3a008e49ad33be94e3c92ccd951e7e5ec8804b9b
unknown <Mohammed@surface.mulhim2.com>
2016-03-19 15:12:50 +0300

    Backup

--- src/Backup/BackupTool.java
+++ src/Backup/BackupTool.java
@@ -31,7 +31,7 @@ public class BackupTool {
          * it is important to mention the directory that you want to store the backup in it after providing the database information
          */
                 executeCmd = "C:\\Program Files\\MySQL\\MySQL Server 5.7\\bin\\mysqldump.exe -u " + dbUser + " -p" +
-                        dbPass + " " + dbName + " -r " + UPLOAD_DIRECTORY +"\\Backup_"+date.toString()+".sql";
+                        dbPass + " " + dbName + " -r \\Backup_"+date.toString()+".sql";
         /**
          * doing the command in windows using Process class and handle the I/O operation by catch
          */
