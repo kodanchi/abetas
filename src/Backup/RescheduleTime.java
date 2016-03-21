7491c9bf3ae251e374e4f9cc354637ed0209612d
unknown <Mohammed@surface.mulhim2.com>
2016-03-21 20:45:49 +0300

    Done with backup

--- src/Backup/RescheduleTime.java
+++ src/Backup/RescheduleTime.java
@@ -6,6 +6,7 @@ import java.sql.Time;
 import java.util.Timer;
 import java.util.TimerTask;
 
+
 /**
  * Created by Mohammed on 3/21/2016.
  */
@@ -17,25 +18,22 @@ public class RescheduleTime extends TimerTask {
     final int DAY = 24 * HOUR;
     final int WEEK = 7 * DAY;
     private Timer timerTime;
+    private String BACKUP_DIRECTORY = null;
     public RescheduleTime(){
         timerTime = new Timer();
     }
+    public RescheduleTime(String bdir){
+        timerTime = new Timer();
+        BACKUP_DIRECTORY = bdir;
+    }
     @Override
     public void run() {
-
+        System.out.println("This is from resche");
+        new BackupTool(BACKUP_DIRECTORY);
     }
-    public void TimerRecschedule(int time){
-
-
-        timerTime.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                TimeClass ti = new TimeClass();
-                ti.cancel();
-
-
-            }
-        },time );
-
-    }
+//    public void TimerRecschedule(int time){
+//
+//
+//        timerTime.schedule(this,MINUTE,time);
+//    }
 }
